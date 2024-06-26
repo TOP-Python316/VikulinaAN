3156b7821657d5f7e1fbb9daefebcc13968e9435
AngelinaVikulina <144624371+AngelinaVikulina@users.noreply.github.com>
2024-06-26 14:32:34 +0300

    Add files via upload

---
+++ HW_2024_06_02/5.sql
@@ -0,0 +1,3 @@
+SELECT name
+FROM MarvelCharacters
+WHERE identify IS NULL OR align IS NULL OR eye IS NULL OR hair IS NULL OR sex IS NULL OR GSM IS NULL OR alive IS NULL OR appearances IS NULL OR year IS NULL;
